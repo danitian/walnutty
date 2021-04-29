eeb99fdaba526944421ca0e30ec649824e2c8dbf
Dani Tian <td.tiandani@gmail.com>
2021-04-29 00:07:34 -0700

    updated database host for deploy

--- database/index.js
+++ database/index.js
@@ -1,9 +1,9 @@
 const { Client } = require('pg');
 
 const client = new Client({
-  host: 'localhost',
-  user: 'dantian',
-  password: 'postgres',
+  host: '54.147.21.125',
+  user: 'postgres',
+  password: 'kK9wI4oK1eH2nR3tN3qK2nS3cS9tN5wE',
   database: 'postgres',
   port: 5432,
   max: 20,
